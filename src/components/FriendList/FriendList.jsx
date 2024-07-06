c8e8d8348f1d2da48d7438d82191a0ba4ba47bba
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-07-06 11:08:29 +0100

    plus

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FriendList = () => {
+  return (
+      <div>
+          <ul>
+            <li><FriendListItem /></li>
+            <li><FriendListItem /></li>
+            <li><FriendListItem /></li>
+            <li><FriendListItem /></li>
+            <li><FriendListItem /></li>
+          </ul>
+    </div>
+  )
+}
+
+export default FriendList
---
+++ src/components/FriensListItem/FriendListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FriendListItem = () => {
+  return (
+      <div>
+          <img src="" alt="Avatar" width="48" />
+          <p>Friend name</p>
+          <p>Friend status</p>
+      </div>
+  )
+}
+
+export default FriendListItem
